eefe0f105ab36a58200b079c6dcbb761efc26ec6
kimcodingvv <junghun1997@naver.com>
2020-05-23 03:04:27 +0900

    Change filename

--- frontend/src/components/user.vue
+++ frontend/src/components/user.vue
@@ -80,7 +80,7 @@ export default {
     .catch(err => console.log(err))
   },
   components: {
-    AddUser: () => import('./AddUser'),
+    AddUser: () => import('./adduser'),
   }
 };
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import User from '@/components/User'
+import Home from '@/components/home'
+import User from '@/components/user'
 
 Vue.use(Router)
 
